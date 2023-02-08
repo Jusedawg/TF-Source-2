2a1e15488142d67ee770bbcb400632decee58fdb
Kilian <noqualitycont@gmail.com>
2023-02-08 20:12:02 +0100

    Create Wearable weapon class

---
+++ code/Player/Weapons/Wearable.cs
@@ -0,0 +1,20 @@
+﻿using Amper.FPS;
+using Sandbox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TFS2
+{
+	[Library("tf_wearable")]
+	public class Wearable : TFWeaponBase, IPassiveChild
+	{
+		public virtual void PassiveSimulate( IClient client )
+		{
+			if ( !EnableDrawing )
+				EnableDrawing = true;
+		}
+	}
+}
