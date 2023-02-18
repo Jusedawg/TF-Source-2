21a1e8289d639b619ec3ac3dfbaa1fd4d7aaf3ab
Kilian <noqualitycont@gmail.com>
2023-02-19 00:44:48 +0100

    Add CaptureTheFlag, ControlPoints gamemode classes

---
+++ code/GameRules/Gamemodes/CaptureTheFlag.cs
@@ -0,0 +1,32 @@
+﻿using Amper.FPS;
+using Sandbox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TFS2
+{
+	public class CaptureTheFlag : IGamemode
+	{
+		public string Title => "Capture The Flag";
+
+		public string Icon => IGamemode.DEFAULT_ICON;
+
+		public CaptureTheFlag()
+		{
+			//EventDispatcher.Subscribe<>
+		}
+
+		public bool HasWon( out TFTeam team, out TFWinReason reason )
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool IsActive()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ code/GameRules/Gamemodes/ControlPoints.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TFS2
+{
+	public class ControlPoints : IGamemode
+	{
+		public string Title => "Control Points";
+
+		public string Icon => "/ui/hud/scoreboard/icon_mode_control.png";
+
+		public ControlPoints()
+		{
+			//EventDispatcher.Subscribe<>
+		}
+
+		public bool HasWon( out TFTeam team, out TFWinReason reason )
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool IsActive()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
