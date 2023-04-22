9ce697359e1fb58d931c118fef480dac67854f27
Gaël R <67245359+KayaDLX@users.noreply.github.com>
2023-04-22 21:35:39 +0200

    Tweak settings styling & slider step, add slider styling

--- code/UI/Menu/Settings/SettingRow.cs
+++ code/UI/Menu/Settings/SettingRow.cs
@@ -59,8 +59,8 @@ internal class SettingRow : Panel
 		{
 			var minmax = property.GetCustomAttribute<MinMaxAttribute>();
 			var min = minmax?.MinValue ?? 0f;
-			var max = minmax?.MaxValue ?? 1000f;
-			var step = property.GetCustomAttribute<SliderStepAttribute>()?.Step ?? .1f;
+			var max = minmax?.MaxValue ?? 100f;
+			var step = property.GetCustomAttribute<SliderStepAttribute>()?.Step ?? 1f;
 			var slider = ValueArea.Add.SliderWithEntry( min, max, step );
 			slider.Bind( "value", target, property.Name );
 			slider.AddEventListener( "value.changed", () =>
