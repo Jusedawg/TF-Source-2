827cee7d2e3e249a6fe30094b2488a11a7870d6f
Kilian <noqualitycont@gmail.com>
2023-06-09 21:49:34 +0200

    Make server name & description fields wrap

---
+++ code/UI/Menu/Lobby/LobbyMenu.razor.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sandbox;
+using Sandbox.UI;
+
+namespace TFS2.Menu;
+
+public partial class LobbyMenu
+{
+	enum LobbyPage
+	{
+		Players,
+		Maps,
+		Settings,
+		Addons
+	}
+
+	LobbyPage Mode;
+
+	public void StartGame()
+	{
+		_ = Game.Menu.EnterServerAsync();
+		this.Navigate( "/" );
+	}
+
+	protected override int BuildHash()
+	{
+		var lobby = Game.Menu.Lobby;
+
+		return HashCode.Combine( lobby?.Title, lobby?.Owner.Id, lobby?.MemberCount, lobby?.MaxMembers, Game.Menu.Package?.PackageSettings?.Count() );
+	}
+
+	public override void Tick()
+	{
+		base.Tick();
+
+		if ( !IsVisible )
+			return;
+
+		if ( Game.Menu.Lobby == null )
+		{
+			this.Navigate( "/" );
+		}
+	}
+
+	void OnClickLobby()
+	{
+		Mode = LobbyPage.Players;
+	}
+
+	void OnClickMap()
+	{
+		Mode = LobbyPage.Maps;
+	}
+
+	void OnClickSettings()
+	{
+		Mode = LobbyPage.Settings;
+	}
+
+	void OnClickAddons()
+	{
+		Mode = LobbyPage.Addons;
+	}
+}
