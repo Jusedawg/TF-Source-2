6a6ee291c340d2b50edd58956adb78be6ee25f65
Kilian <noqualitycont@gmail.com>
2023-07-06 20:11:29 +0200

    Show player life state & respawn timer in round status section

---
+++ code/UI/HUD/Objectives/RoundStatusPlayers.razor.cs
@@ -0,0 +1,48 @@
+﻿using Sandbox;
+using Sandbox.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TFS2.UI;
+
+public partial class RoundStatusPlayers : Panel
+{
+	public TFTeam Team { get; set; }
+	public Dictionary<IClient, RoundStatusPlayersEntry> Players { get; set; } = new();
+
+	public RoundStatusPlayers()
+	{
+		BindClass( "red", () => Team == TFTeam.Red );
+		BindClass( "blue", () => Team == TFTeam.Blue );
+	}
+
+	public override void Tick()
+	{
+		var teamClients = Sandbox.Game.Clients.Where( x => x.GetTeam() == Team );
+		var keyClients = Players.Keys;
+
+		foreach ( var client in teamClients.Except( keyClients ) ) AddClient( client );
+		foreach ( var client in keyClients.Except( teamClients ) ) RemoveClient( client );
+	}
+
+	public void AddClient( IClient client )
+	{
+		Players[client] = new RoundStatusPlayersEntry
+		{
+			Client = client,
+			Parent = this
+		};
+	}
+
+	public void RemoveClient( IClient client )
+	{
+		if ( Players.TryGetValue( client, out var entry ) )
+		{
+			entry?.Delete( true );
+			Players.Remove( client );
+		}
+	}
+}
---
+++ code/UI/HUD/Objectives/RoundStatusPlayersEntry.razor.cs
@@ -0,0 +1,65 @@
+﻿using Sandbox.UI;
+using Sandbox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace TFS2.UI;
+
+public partial class RoundStatusPlayersEntry : Panel
+{
+	public IClient Client { get; set; }
+	Panel Portrait;
+	Label RespawnTimer;
+
+	public override void Tick()
+	{
+		if ( Portrait == null ) return;
+
+		var local = Game.LocalClient;
+		if ( local == null )
+			return;
+
+		var ply = Client.Pawn as TFPlayer;
+		if(ply == null ) 
+			return;
+
+		var ourTeam = local.GetTeam();
+		var theirTeam = Client.GetTeam();
+		var anonymous = true;
+
+		if ( ourTeam == theirTeam )
+		{
+			var theirClass = Client.GetPlayerClass();
+			if ( theirClass != null )
+			{
+				anonymous = false;
+				Portrait.Style.Set( "background-image", $"url(/ui/hud/classportraits/{theirClass.Title}_{theirTeam.GetName()}.png)" );
+			}
+		}
+
+		if ( anonymous )
+			Portrait.Style.Set( "background-image", $"url(/ui/hud/classportraits/silhouette.png)" );
+
+		else
+		{
+			var rules = TFGameRules.Current;
+			if(!ply.IsAlive && rules.CanTeamRespawn(ply.Team) && rules.CanPlayerRespawn(ply) && !TFGameRules.Current.IsWaitingForPlayers)
+			{
+				var respawnTime = TFGameRules.Current.GetNextPlayerRespawnWaveTime( ply ) - Time.Now;
+				RespawnTimer.Text = respawnTime.CeilToInt().ToString();
+				RespawnTimer.SetClass( "visible", true );
+			}
+			else
+			{
+				RespawnTimer.SetClass( "visible", false );
+			}
+		}
+
+		Portrait.SetClass( "dead", !Client.IsAlive() );
+		Portrait.SetClass( "us", local == Client );
+	}
+}
